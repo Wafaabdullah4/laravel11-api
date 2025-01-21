4103ab1df140f01ec63762de82c7175c5e474ab5
Wafa Abdullah Nurbani Adam <menerang12345678@gmail.com>
2025-01-21 09:55:07 +0700

    feat: vercel deploy

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 // menambahkan route post disini
 
